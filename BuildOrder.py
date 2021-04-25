4dc85d9011bf32149edba188593c6ce4f54bf19a
steffannunez <brandon.nunez@usach.cl>
2021-04-25 00:08:24 -0400

    inicio

---
+++ BuildOrder.py
@@ -0,0 +1,34 @@
+#imports
+
+#definicion de clases
+
+class edificio:
+    nombre=""
+    costoMineral=0
+    costoGas=0
+    costoCapacidad=0
+    costoTiempo=0
+
+class unidad:
+    nombre=""
+    costoMineral=0
+    costoGas=0
+    costoCapacidad=0
+    costoTiempo=0
+    daño=0
+    recoleccion=0
+    vida=0
+    defensa=0
+
+#datos globales
+
+MINERAL=0
+GAS=0
+CAPACIDAD=0
+
+#funciones
+
+leerArchivo()
+escribirArchivo()
+
+#algoritmo
