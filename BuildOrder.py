1cf3f991107216c082ef4d6d5f3f96030dea8215
brandon nuñez <brandon.nunez@usach.cl>
2021-04-27 20:16:04 -0400

    detalles

--- BuildOrder.py
+++ BuildOrder.py
@@ -9,26 +9,82 @@ class edificio:
     costoCapacidad=0
     costoTiempo=0
 
+    def __init__(self, nombre, costoMineral, costoGas, costoCapacidad, costoTiempo):
+        self.nombre = nombre
+        self.costoMineral= costoMineral
+        self.costoGas=costoGas
+        self.costoCapacidad=costoCapacidad
+        self.costoTiempo=costoTiempo
+    def mostrarClase(self):
+        print(self.nombre)
+
 class unidad:
     nombre=""
     costoMineral=0
     costoGas=0
     costoCapacidad=0
     costoTiempo=0
-    daño=0
+    damage=0
     recoleccion=0
     vida=0
     defensa=0
+    origen=""
 
-#datos globales
+    def __init__(self, nombre, costoMineral, costoGas, costoCapacidad, costoTiempo, damage, recoleccion, vida, defensa, origen):
+        self.nombre = nombre
+        self.costoMineral= costoMineral
+        self.costoGas=costoGas
+        self.costoCapacidad=costoCapacidad
+        self.costoTiempo=costoTiempo
+        self.damage = damage
+        self.recoleccion = recoleccion
+        self.vida = vida
+        self.defensa = defensa
+        self.origen = origen
+    def mostrarClase(self):
+        print(self.nombre)
+#datos INICIALES
 
 MINERAL=0
 GAS=0
 CAPACIDAD=0
-
+SEGUNDOS=0
+recolectores=6
+unidades=[]
 #funciones
 
-leerArchivo()
-escribirArchivo()
 
+def leerArchivo(listaE, listaU, nombreArchivo):
+    f = open ('prueba.txt','r')
+    for linea in f:
+        if (linea[0]=="E"):
+            line2=linea.split(" ")
+            e=edificio(line2[1],int(line2[2]),int(line2[3]),int(line2[4]),int(line2[5]))
+            listaE.append(e)
+        if (linea[0]=="U"):
+            line2=linea.split(" ")
+            u=unidad(line2[1],int(line2[2]),int(line2[3]),int(line2[4]),int(line2[5]),int(line2[6]),int(line2[7]),int(line2[8]), int(line2[9]),line2[10])
+            listaU.append(u)
+    f.close()
+    return
+def escribirArchivo():
+    return
+
+def tecnologiaTanto():
+    return
+
+def mostrarlistas(lista):
+    fila="elementos: "
+    for elemento in lista:
+        fila = fila+", " + elemento.nombre
+    print( fila)
 #algoritmo
+#listas temporales
+listaEdificios=[]
+listaUnidades=[]
+nombreArchivo= input("Ingrese el nombre del archivo que desea usar: ")
+leerArchivo(listaEdificios,listaUnidades, nombreArchivo+".txt")
+mostrarlistas(listaUnidades)
+mostrarlistas(listaEdificios)
+#lista objetivo
+buildOrder=[]
